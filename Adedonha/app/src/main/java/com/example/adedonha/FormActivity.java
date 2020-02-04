66f6dcc91a2ed9444ac8332a624b440dce99a50c
Jeremias Neres <jeremiasneres23@gmail.com>
2020-02-04 17:54:28 -0300

    Construindo as acoes do app

--- Adedonha/app/src/main/java/com/example/adedonha/FormActivity.java
+++ Adedonha/app/src/main/java/com/example/adedonha/FormActivity.java
@@ -2,13 +2,105 @@ package com.example.adedonha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
+
+    TextView txtDescricao;
+    EditText txtFruta;
+    EditText txtPaises;
+    EditText txtAnimal;
+    Button btnEnviarForm;
+
+    String letraEscolhida;
+    boolean dadosValidados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        txtDescricao = findViewById(R.id.descricao);
+        btnEnviarForm = findViewById(R.id.butaoEnviarForm);
+
+        txtFruta = findViewById(R.id.fruta);
+        txtPaises = findViewById(R.id.pais);
+        txtAnimal = findViewById(R.id.animal);
+
+        //Recebe a letra e atribui ao text view
+        Intent receber = getIntent();
+        letraEscolhida = receber.getStringExtra("key_letra");
+        txtDescricao.setText("Começãndo com a letra " + letraEscolhida + " \n digite o nome de um(a):");
+
+        //Envia os dados preechidos no formulário
+        btnEnviarForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dadosValidados = validarFormulario();
+
+                if(dadosValidados) {
+                    Intent i = new Intent(getApplicationContext(), ResultadoActivity.class);
+                    i.putExtra("key_fruta", txtFruta.getText().toString());
+                    i.putExtra("key_pais", txtPaises.getText().toString());
+                    i.putExtra("key_animal", txtAnimal.getText().toString());
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(getApplication(), "Preencha os campos", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
     }
+
+    private boolean validarFormulario(){
+
+        boolean retorno = false;
+        if(!TextUtils.isEmpty(txtFruta.getText().toString()) && !verificarPrimeiraLetra(txtFruta.getText().toString())){
+            retorno = true;
+        }else{
+            txtFruta.setError("*");
+            txtFruta.requestFocus();
+        }
+
+        if(!TextUtils.isEmpty(txtAnimal.getText().toString()) && !verificarPrimeiraLetra(txtAnimal.getText().toString())){
+            retorno = true;
+        }else{
+            txtAnimal.setError("*");
+            txtAnimal.requestFocus();
+        }
+
+        if(!TextUtils.isEmpty(txtPaises.getText().toString()) && !verificarPrimeiraLetra(txtPaises.getText().toString())){
+            retorno = true;
+        }else{
+            txtPaises.setError("*");
+            txtPaises.requestFocus();
+        }
+
+        return retorno;
+    }
+
+    private boolean verificarPrimeiraLetra(String palavra){
+
+        boolean retorno = false;
+
+        int i = palavra.indexOf(letraEscolhida);
+
+        if(i == 0){
+            retorno = true;
+        }
+
+        return retorno;
+
+    }
+
 }
--- Adedonha/app/src/main/java/com/example/adedonha/MainActivity.java
+++ Adedonha/app/src/main/java/com/example/adedonha/MainActivity.java
@@ -3,22 +3,47 @@ package com.example.adedonha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     String letras[] = {"A","E","I","O","U"};
+    TextView letrasTxt;
+    Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        letrasTxt = findViewById(R.id.letraRandon);
+        nextPage = findViewById(R.id.btnAvancar);
+
         //Embaralhando as letras atráves da posição
-        Toast.makeText(this, letras[(int)Math.random()], Toast.LENGTH_LONG).show();
+        Random random = new Random();
+        int res = random.nextInt(letras.length);
+        letrasTxt.setText(letras[res]);
 
+        //Toast.makeText(this, letras[res], Toast.LENGTH_LONG).show();
 
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Envia para a FormActivity
+                Intent i = new Intent(getApplicationContext(),FormActivity.class);
+
+                i.putExtra("key_letra", letrasTxt.getText().toString());
+                startActivity(i);
+                finish();
+            }
+        });
 
     }
 }
