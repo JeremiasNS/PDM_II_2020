a732277f03e750f363625c8397f57f9e9bb36a64
Jeremias <jeremias@Jeremias.neres.net.br>
2020-02-04 10:36:54 -0300

    Random na MainActivity

--- Adedonha/app/src/main/java/com/example/adedonha/MainActivity.java
+++ Adedonha/app/src/main/java/com/example/adedonha/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.adedonha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String letras[] = {"A","E","I","O","U"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Embaralhando as letras atráves da posição
+        Toast.makeText(this, letras[(int)Math.random()], Toast.LENGTH_LONG).show();
+
+
+
     }
 }
