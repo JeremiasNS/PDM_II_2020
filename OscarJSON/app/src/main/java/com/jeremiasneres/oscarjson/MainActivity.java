3956938ad08ced698e5a46e2b3daeb7325f242fc
Jeremias Neres <jeremiasneres23@gmail.com>
2020-02-17 10:45:50 -0300

    Criando  novas activity e implementando novas funcionalidades

--- OscarJSON/app/src/main/java/com/jeremiasneres/oscarjson/MainActivity.java
+++ OscarJSON/app/src/main/java/com/jeremiasneres/oscarjson/MainActivity.java
@@ -3,12 +3,30 @@ package com.jeremiasneres.oscarjson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nome;
+    EditText direcao;
+    EditText categoria;
+    Button cadastrar;
+    Button enviar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
